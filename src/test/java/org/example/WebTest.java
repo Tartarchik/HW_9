4029a5c935a8ba6ee0ceb9e2a4ceec9af2eaad71
renat <renat1703@gmail.com>
2023-04-07 15:34:17 +0300

    Fix

--- src/test/java/org/example/WebTest.java
+++ src/test/java/org/example/WebTest.java
@@ -3,7 +3,6 @@ package org.example;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
